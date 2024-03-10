1bbcb746da9f20f6c6f78c5c29c9f4fe3a3d987f
Gabriel Fernando <Biel@MacBook-Pro-de-Melanie.local>
2024-03-10 17:54:37 -0300

    primeiro commit

---
+++ src/main/java/br/gabriel/gerenciamento/carros/Gerenciamento/de/Carros/GerenciamentoDeCarrosApplication.java
@@ -0,0 +1,13 @@
+package br.gabriel.gerenciamento.carros.Gerenciamento.de.Carros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeCarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeCarrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gabriel/gerenciamento/carros/Gerenciamento/de/Carros/GerenciamentoDeCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gabriel.gerenciamento.carros.Gerenciamento.de.Carros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
